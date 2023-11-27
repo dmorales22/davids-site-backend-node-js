43b4fc2443b8e3f7667fedd257440600ce99bfe5
David Morales <sloshedtrain@gmail.com>
2023-11-27 01:31:22 -0700

    A small change to server.js

--- server.js
+++ server.js
@@ -4,12 +4,9 @@ const app = require("./index");
 dotenv.config({ path: "./.env" });
 const port = process.env.PORT;
 
-// COMMENT FOR DEPLOYMENT ONLY. ENABLES SSL BUT REQUIRES ADMIN/ROOT PRIVILEGES
-if (process.env.DEPLOYMENT === "0") {
-  app.listen(port, () => {
-    console.log(`Now listening on port ${port}`);
-  });
-}
+app.listen(port, () => {
+  console.log(`Now listening on port ${port}`);
+});
 
 // Close database connection when the app is terminated.
 process.on("SIGINT", function () {
